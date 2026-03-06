4d7f6188858b8c091668d34d2ad0099c2f55874e
nergizyilmaz <yilmaz.nergiz37@ibu.edu.mk>
2026-03-06 10:23:34 +0000

    finished search1

--- src/search1.c
+++ src/search1.c
@@ -18,3 +18,30 @@ Rules:
 - You must use strcmp().
 - Do NOT use == for string comparison.
 */
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char names[][50] = {"Ali", "Ayse", "Mehmet", "Fatma", "Can"};
+    int size = sizeof(names) / sizeof(names[0]);
+    char search[50];
+    int found = 0;
+
+    printf("Enter a name to search: ");
+    scanf("%s", search);
+
+    for (int i = 0; i < size; i++) {
+        if (strcmp(search, names[i]) == 0) {
+            found = 1;
+            break;
+        }
+    }
+
+    if (found) {
+        printf("Found\n");
+    } else {
+        printf("Not found\n");
+    }
+
+    return 0;
+}
