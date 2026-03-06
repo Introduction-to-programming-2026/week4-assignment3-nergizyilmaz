2cf097e37bfddeea6417e4d8bc5bc71c13786d74
nergizyilmaz <yilmaz.nergiz37@ibu.edu.mk>
2026-03-06 10:21:08 +0000

    finished search0

--- src/search0.c
+++ src/search0.c
@@ -17,3 +17,29 @@ Rules:
 - You must use a loop.
 - Do NOT use any built-in search function.
 */
+#include <stdio.h>
+
+int main() {
+    int numbers[] = {3, 7, 12, 19, 25, 8, 5};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+    int target;
+    int found = 0;
+
+    printf("Enter a number to search: ");
+    scanf("%d", &target);
+
+    for (int i = 0; i < size; i++) {
+        if (numbers[i] == target) {
+            found = 1;
+            break;
+        }
+    }
+
+    if (found) {
+        printf("Found\n");
+    } else {
+        printf("Not found\n");
+    }
+
+    return 0;
+}
